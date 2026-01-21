55a3c7a6804a0acf76e5de78a4b879c71c96b8bc
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-21 19:25:00 +0530

    Update config.py
    Signed-off-by: nrecover4587-rgb <nrecover4587@gmail.com>

--- config.py
+++ config.py
@@ -44,8 +44,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/amigr8")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/randomlychats")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+Da5wN0OMdVc1Mjg0")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+Da5wN0OMdVc1Mjg0")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
@@ -89,19 +89,19 @@ autoclean = []
 confirmer = {}
 file_cache: dict[str, float] = {}
 
-START_IMG_URL = ["https://te.legra.ph/file/5fd13f2cc0d03bce9f7f2.jpg",
-                 "https://te.legra.ph/file/c15d01b3e6b40ea141dc9.jpg",
-                 "https://te.legra.ph/file/5fd13f2cc0d03bce9f7f2.jpg"]
+START_IMG_URL = ["https://files.catbox.moe/scu5wb.jpg",
+                 "https://files.catbox.moe/scu5wb.jpg",
+                 "https://files.catbox.moe/scu5wb.jpg"]
     
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/87f680aead03443f291b0.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/scu5wb.jpg"
 )
-PLAYLIST_IMG_URL = "https://graph.org/file/c95a687e777b55be1c792.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/edd388a42dd2c499fd868.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/492a3bb2e880d19750b79.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/492a3bb2e880d19750b79.jpg"
-STREAM_IMG_URL = "https://graph.org/file/ff2af8d4d10afa1baf49e.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/c95a687e777b55be1c792.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/scu5wb.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/scu5wb.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/scu5wb.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/scu5wb.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/scu5wb.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/scu5wb.jpg"
 YOUTUBE_IMG_URL = "https://graph.org/file/e8730fdece86a1166f608.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/0bb6f36796d496b4254ff.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/0bb6f36796d496b4254ff.jpg"
